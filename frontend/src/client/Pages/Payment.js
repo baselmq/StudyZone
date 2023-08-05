702fef34826c6509e61d69553406f1777873a71a
munib dalati <munib.dalati@gmail.com>
2023-08-05 16:09:04 +0300

    payment validation

--- frontend/src/client/Pages/Payment.js
+++ frontend/src/client/Pages/Payment.js
@@ -1,42 +1,73 @@
 import React from "react";
 import "../Styles/App.css";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 const Payment = () => {
   let navigate = useNavigate();
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Perform your validation here
+    if (isFormValid()) {
+      // If the form is valid, proceed with navigation
+      navigate("/paymentSuccess");
+    }
+  };
+
+  const isFormValid = () => {
+    const form = document.querySelector(".payment-form");
+
+    // Check if all the form elements are valid
+    return form.checkValidity();
+  };
+
   return (
     <div className="login_container">
-      <form className="login-form">
+      <form
+        className="login-form payment-form needs-validation"
+        novalidate
+        onSubmit={handleFormSubmit}
+      >
         <div className="column">
           <div className="input-groups_title">
             <h1>Payment details</h1>
           </div>
           {/* Name */}
           <div className="input-groups">
-            <label htmlFor="username">Name on card</label>
+            <label for="validationTooltip01" class="form-label">
+              Name on card
+            </label>
             <input
               type="text"
-              id="text"
-              name="text"
-              placeholder="Enter your name as appearing on your card"
-              required=""
+              class="form-control"
+              id="validationTooltip01"
+              required
             />
+            <div class="valid-tooltip">Looks good!</div>
           </div>
+
           {/* Card number */}
           <div className="input-groups">
-            <label htmlFor="number">Credit Card Number</label>
+            <label for="validationTooltip01" class="form-label">
+              Credit Card Number
+            </label>
             <input
               type="text"
-              id="number"
-              name="number"
+              class="form-control"
+              id="validationTooltip01"
+              required
               placeholder="XXXX XXXX XXXX XXXX"
-              required=""
+              pattern="[0-9]{16}"
+              title="Credit card number must be 16 digits"
               maxLength="16"
             />
+            <div class="valid-tooltip">Looks good!</div>
           </div>
+
           {/* date & cvv */}
           <div className="input-groups expiry-cvv">
+            {/* expiry date */}
             <div>
               <label htmlFor="expiry-date">Expiry Date:</label>
               <input
@@ -45,20 +76,28 @@ const Payment = () => {
                 name="expiry-date"
                 placeholder="MM/YY"
                 maxLength="5"
+                pattern="(0[1-9]|1[0-2])\/\d{2}"
+                title="Expiry date must be in the format MM/YY"
                 required
               />
             </div>
+            {/* CVV */}
 
-            <div className="">
-              <label htmlFor="number">CVV</label>
+            <div>
+              <label for="validationTooltip01" class="form-label">
+                CVV
+              </label>
               <input
                 type="text"
-                id="text"
-                name="text"
+                class="form-control"
+                id="validationTooltip01"
+                required
                 placeholder="3-number digets"
+                pattern="[0-9]{3}"
+                title="CVV must be a 3-digit number"
                 maxLength="3"
-                required=""
               />
+              <div class="valid-tooltip">Looks good!</div>
             </div>
           </div>
 
@@ -85,13 +124,9 @@ const Payment = () => {
 
           {/* payment btn */}
           <div className="input-groups">
-            <Link to={"/paymentSuccess"}>
-              {" "}
-              <button className="btn-login mb-1" type="submit">
-                Make Payment
-              </button>
-            </Link>
-            
+            <button className="btn-login mb-1" type="submit">
+              Make Payment
+            </button>
           </div>
         </div>
         <div className="payment_column_img"></div>
